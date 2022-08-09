09a9ebd6fe00384e5b4beb42b6509beadcbb8637
Arghya Deep Pal <arghyadeep100@gmail.com>
2022-08-09 21:24:05 +0530

    E-Hub Added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,9 +4,7 @@ import Carousel from 'react-bootstrap/Carousel';
     
 const Projects=()=>{
 
-    const movies=["Movies-1","Movies-2","Movies-3","Movies-4","Movies-5","Movies-6"];
-
-    const tvSeries=["Tv-Series-1","Tv-Series-2","Tv-Series-3","Tv-Series-4","Tv-Series-5","Tv-Series-6","Tv-Series-7","Tv-Series-8"];
+    const eHub=["E-Hub-1", "E-Hub-2", "E-Hub-3", "E-Hub-4", "E-Hub-5", "E-Hub-6", "E-Hub-7", "E-Hub-8", "E-Hub-9", "E-Hub-10", "E-Hub-11", "E-Hub-12"];
 
     const resume=["Online-Resume-1","Online-Resume-2","Online-Resume-3","Online-Resume-4"];
 
@@ -19,7 +17,7 @@ const Projects=()=>{
             
             <Col lg={6} style={{marginBottom:'30px'}}>
                 <Carousel indicators={false}>
-                    {tvSeries.map((src)=>(
+                    {eHub.map((src)=>(
                         <Carousel.Item style={{border:"2px solid black"}}>
                             <img className="d-block w-100" src={`/assets/${src}.png`} alt={src}/>
                         </Carousel.Item>
@@ -27,15 +25,18 @@ const Projects=()=>{
                 </Carousel>
             </Col>
             <Col lg={6} style={{border:'2px solid black', boxShadow:'5px 5px 3px black'}}>
-                <h3>TV SERIES APP</h3>
-                <h5><a href="https://tv-series-app-arghyadeep7.vercel.app/tv/trending" style={{textDecoration:'none'}} target="_blank"><i class="fas fa-code"></i>&nbsp;Website Link</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Arghyadeep7/TV-Series-App" style={{textDecoration:'none'}} target="_blank"><i class="fab fa-github"></i>&nbsp;Repo Link</a></h5>
-                <h6>User-friendly and easy to use TV-Series App</h6>
+                <h3>E-HUB | Entertainment Hub</h3>
+                <h5><a href="https://e-hub-arghyadeep7.vercel.app/" style={{textDecoration:'none'}} target="_blank"><i class="fas fa-code"></i>&nbsp;Website Link</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Arghyadeep7/E-Hub" style={{textDecoration:'none'}} target="_blank"><i class="fab fa-github"></i>&nbsp;Repo Link</a></h5>
+                <h6>Your one destination for all Movies & TV/Web Series</h6>
                 <ul>
                     <li>
-                        Filter by : Trending, Popular, Top-rated
+                        Filter by : Now Playing, Upcoming, Trending, Popular, Top-rated
                     </li>
                     <li>
-                        Search any TV/Web Series
+                        Search any movie or/and TV/Web Series
+                    </li>
+                    <li>
+                        Get details of any movie
                     </li>
                     <li>
                         Get details of any season of any TV/Web Series
@@ -44,50 +45,13 @@ const Projects=()=>{
                         Get details of any episode of any season of any TV/Web Series
                     </li>
                     <li>
-                        Get Casts of any season,episode and of any TV/Web Series
+                        Get Casts of any movie or any season,episode of any TV/Web Series
                     </li>
                     <li>
                         Get all related Images, Posters, Trailers and Video links
                     </li>
                     <li>
-                        Get similar related TV/Web Series
-                    </li>
-                </ul>
-                <h5>React.js | TMDB API | React-Bootstrap | Axios | CSS</h5>
-            </Col>
-              
-        
-            <Col lg={6} style={{marginTop:'30px'}}>
-                <Carousel indicators={false}>
-                    {movies.map((src)=>(
-                        <Carousel.Item style={{border:"2px solid black"}}>
-                            <img className="d-block w-100" src={`/assets/${src}.png`} alt={src}/>
-                        </Carousel.Item>
-                    ))}
-                </Carousel>
-            </Col>
-            <Col lg={6} style={{marginTop:'30px', border:'2px solid black', boxShadow:'5px 5px 3px black'}}>
-                <h3>MOVIES APP</h3>
-                <h5><a href="https://movies-app-arghyadeep7.vercel.app/movies/now_playing" style={{textDecoration:'none'}} target="_blank"><i class="fas fa-code"></i>&nbsp;Website Link</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Arghyadeep7/Movies-App" style={{textDecoration:'none'}} target="_blank"><i class="fab fa-github"></i>&nbsp;Repo Link</a></h5>
-                <h6>User-friendly and easy to use Movies App</h6>
-                <ul>
-                    <li>
-                        Filter by : Now Playing, Upcoming, Trending, Popular, Top-rated
-                    </li>
-                    <li>
-                        Search any Movie
-                    </li>
-                    <li>
-                        Get all details of any Movie
-                    </li>
-                    <li>
-                        Get Casts of any Movie
-                    </li>
-                    <li>
-                        Get all related Images, Trailers and Video links
-                    </li>
-                    <li>
-                        Get similar related Movies
+                        Get similar related Movies or TV/Web Series
                     </li>
                 </ul>
                 <h5>React.js | TMDB API | React-Bootstrap | Axios | CSS</h5>
